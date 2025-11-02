47469545494e5709efff31761b4793af8e9a5824
Om’s Mac-Mini <contact@omshejul.com>
2025-11-02 09:21:37 +0530

    Initial Commit

---
+++ PDFTools/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PDFTools
+//
+//  Created by Om Shejul on 02/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PDFTools/PDFToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFToolsApp.swift
+//  PDFTools
+//
+//  Created by Om Shejul on 02/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PDFToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
